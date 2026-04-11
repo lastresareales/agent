e08e3a31e4d22092d75189123ac0a19ca8cee15e
Athos Turner <athost6873@bigfoot.spokane.edu>
2026-04-10 17:40:36 -0700

    Create GraphBuilder class for knowledge graph

---
+++ knowledge_graph.py
@@ -0,0 +1,10 @@
+# knowledge_graph.py
+
+class GraphBuilder:
+    def __init__(self):
+        self.nodes = []
+        self.edges = []
+        
+    def add_relationship(self, entity1, relation, entity2):
+        # Logic to map out how extracted entities connect to each other
+        pass
