b26a1b5e20e513bb922abbef770423b1c7e1abc8
Athos Turner <athost6873@bigfoot.spokane.edu>
2026-04-10 17:39:02 -0700

    Refactor EntityDataset to NERDataset with JSON support

--- entity_dataset.py
+++ entity_dataset.py
@@ -1,11 +1,20 @@
-import pandas as pd
+# entity_dataset.py
+import json
+import torch
+from torch.utils.data import Dataset
+from config import BATCH_SIZE
 
-class EntityDataset(torch.utils.data.Dataset):
-    def __init__(self, data):
-        self.data = pd.DataFrame(data)
-
-    def __getitem__(self, idx):
-        return self.data.iloc[idx]
-
+class NERDataset(Dataset):
+    def __init__(self, filepath, tokenizer):
+        self.tokenizer = tokenizer
+        # Load the dataset.json
+        with open(filepath, 'r') as f:
+            self.data = json.load(f)
+            
     def __len__(self):
         return len(self.data)
+        
+    def __getitem__(self, idx):
+        # Logic to grab a single sentence and its labels, 
+        # tokenize it, and return PyTorch tensors.
+        pass
